27943669969187ca82ee4372578b03fa8d8567a4
Glitch (glitch-hello-node) <none>
2022-07-30 13:57:46 +0000

    💰⭐️ Checkpoint ./script.js:26026384/652 ./index.html:26026384/1075

--- script.ts
+++ script.ts
@@ -10,4 +10,12 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-window.document.write("Rodando script")
+document.write("Rodando script")
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
+  });
+});
